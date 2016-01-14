a4329ca89d8718f1e7d5f5540bc3726112625534
ele7enxxh <ele7enxxh@qq.com>
2016-01-14 11:52:58 +0800

    Update asm.c

--- asm.c
+++ asm.c
@@ -15,7 +15,7 @@ int inline asmCacheFlush(unsigned int begin, unsigned int end)
 		"mov     %[ret], r0\n\t"
 		"pop    {r1, r2, r7}\n\t"
 
-		:   [ret] "=r" (ret)
+		:	[ret] "=r" (ret)
 		:	[begin] "r" (begin), [end] "r" (end), [syscall] "r" (syscall)
 		:	"r0", "r1", "r2", "r7"
 	);
@@ -41,7 +41,7 @@ void inline *asmMmap2(void *addr, size_t length, int prot, int flags, int fd, of
 		"mov     %[ret], r0\n\t"
 		"pop    {r1, r2, r3, r4, r5, r7}\n\t"
 
-		:   [ret] "=r" (ret)
+		:	[ret] "=r" (ret)
 		:	[addr] "r" (addr), [length] "r" (length), [prot] "r" (prot), [flags] "r" (flags), [fd] "r" (fd), [pgoffset] "r" (pgoffset), [syscall] "r" (syscall)
 
 	);
